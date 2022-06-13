618713dee3d812c9c264089d5d70f6f735814d1e
StyxSlayer <83481046+StyxSlayer@users.noreply.github.com>
2022-06-12 18:03:03 -0700

    fixed standby field clicker

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
 let state = "init";
+var img = new Image();
+img.src = '/img/field.png';
+var canvas = document.getElementById('fieldCanvas');
+var ctx = canvas.getContext('2d');
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.drawImage(img, 0, 0);
+document.getElementById("fieldCanvas").addEventListener("click", ()=>{
+    canvasClicked()
+})
+    
 
-window.onload = function() {
-    var img = new Image();
-    img.src = '/img/field.png';
-    var canvas = document.getElementById('fieldCanvas');
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0);
-    img.onload = function () {
-    document.getElementById("fieldCanvas").addEventListener("click", ()=>{
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0);
-        var pos = getMousePos(canvas, event);
-        ctx.strokeStyle = "white";
-        ctx.fillStyle = "white";
-        ctx.beginPath();
-        ctx.arc(pos.x, pos.y, 10, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.stroke();
-        autoPos[0] = Math.round(pos.x);
-        autoPos[1] = Math.round(pos.y);
-        console.log("canvas clicked, x: " + autoPos[0] + ", y: " + autoPos[1]);
-    })
-    }
-}
 
 document.getElementById("initBtn").addEventListener("click", ()=>{
     transition(0);
 
-})
-document.getElementById("standbyLabel").addEventListener("click", ()=>{
-    if(state == "standby") {
-        transition(1);
-    }
 })
 
 function getMousePos(canvas, evt) {
@@ -44,10 +24,23 @@ function getMousePos(canvas, evt) {
     };
 }
 
+function canvasClicked(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+    var pos = getMousePos(canvas, event);
+    ctx.strokeStyle = "white";
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(pos.x, pos.y, 5, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    console.log("canvas clicked, x: " + Math.round(pos.x) + ", y: " + Math.round(pos.y));
+}
+
 function transition(i){
     if(i==0 && state == "init"){
-        
-        
         document.getElementById("initFormContainer").classList.add("transitionEvent0");
         setTimeout(()=>{
             document.getElementById("initFormContainer").classList.add("hideClass");
@@ -56,7 +49,7 @@ function transition(i){
         document.getElementById("standbyContainer").classList.add("transitionEvent0Rev");
         setTimeout(()=>{
             document.getElementById("standbyContainer").style.display = "flex";
-
+            canvasClicked()
         }, 1000)
         state = "standby"
         return;
